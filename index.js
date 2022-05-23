788d279ee3ae9251f2e14fad55e81c508548f6cb
sobhan <somayahshams1995@gmail.com>
2022-05-23 04:32:35 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const divide = function(a,b) {
+    return a/b;
+  }
+
+  const square = (a) => {
+     return a*a;
+  }
+
+  const add =  (parameter1, parameter2) => {
+    console.log(`Adding ${parameter1}`);
+    console.log(`Adding ${parameter2}`);
+    return parameter1 + parameter2;
+  }
+  
